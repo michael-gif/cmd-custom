e0f2b62fd7a29e440d55bf933612d5277d34eda7
michael-gif <53706169+michael-gif@users.noreply.github.com>
2020-03-15 18:12:37 +0000

    Add files via upload

---
+++ file_editor.py
@@ -0,0 +1,30 @@
+def fileedit():
+    import tkinter as tk
+    from tkinter import filedialog
+    window2 = tk.Tk()
+    window2.title('custom_cmd:file-editor')
+    window2.geometry('647x343')
+    window2.configure(bg='black')
+    name = ''
+    def openfile():
+        global name
+        name = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+        with open(name,'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                t.insert(tk.INSERT,line)
+    def savefile():
+        global name
+        with open(name,'w') as f:
+            text = t.get('1.0','end-1c')
+            f.write(text)
+    t = tk.Text(window2,bg='black',fg='white',insertbackground='white',state="normal")
+    t.place(x=0,y=0,width=647,height=320)
+    b = tk.Button(window2,bg='grey25',text='Open',command=openfile)
+    b.place(x=0,y=318,width=323)
+    b2 = tk.Button(window2,bg='grey25',text='Save',command=savefile)
+    b2.place(x=324,y=318,width=323)
+    tk.mainloop()
+
+if __name__ == "__main__":
+    fileedit()
